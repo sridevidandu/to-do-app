74f23030b348682d271d797cb39d73dadeea828f
Sridevi <SiriPandu@sridevis-mbp.lan>
2020-07-07 12:21:36 -0400

    Styled the delete icon

--- src/pages/listItems.js
+++ src/pages/listItems.js
@@ -4,13 +4,14 @@ const ListItems = (props) => {
 
     const items = props.items;
 
-    return(
+    return (
         <div>
             {items.map(item => {
-                return <div className ="list" key="item.key">
-                        <p>{item.text}</p>
-                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                return <div className="list" key="item.key">
+                    <div className = "item">{item.text}
+                        <span class="fa fa-trash-o" aria-hidden="true"></span>
                     </div>
+                </div>
             })}
         </div>
     )
