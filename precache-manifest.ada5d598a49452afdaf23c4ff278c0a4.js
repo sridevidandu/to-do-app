ca9dcd5d5d792039e2b55cbce54ec7edd1c12af3
Sridevi <SiriPandu@sridevis-mbp.lan>
2020-07-09 10:23:37 -0400

    Updates

---
+++ precache-manifest.ada5d598a49452afdaf23c4ff278c0a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fb00e25e5e8ff05b85d3cc3aaf9194",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "27d655be11797f05c95e",
+    "url": "/to-do/static/css/main.cc733e2f.chunk.css"
+  },
+  {
+    "revision": "403d05fd3c3aa0d2de84",
+    "url": "/to-do/static/js/2.702f662e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to-do/static/js/2.702f662e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d655be11797f05c95e",
+    "url": "/to-do/static/js/main.d39d36f5.chunk.js"
+  },
+  {
+    "revision": "6390033f780efff2fa21",
+    "url": "/to-do/static/js/runtime-main.fcc88f23.js"
+  }
+]);
